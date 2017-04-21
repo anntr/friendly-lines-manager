13f5e96bb4f18ca38b03cccee104e5903d8721bc
anntr <anna.trapczynska@senti1.com>
2017-04-21 21:30:54 +0200

    add native-base framework

--- app/containers/MainPage.js
+++ app/containers/MainPage.js
@@ -1,7 +1,12 @@
-import React from "react"
-import {Text, View} from "react-native"
+import React from "react";
+import {Container, Content, Text, Header, Body, Title, Button} from 'native-base';
 
-export default MainPage = ({}) =>
-    <View>
-        <Text>Welcome to Main page</Text>
-    </View>
+export default MainPage = () =>
+    <Container>
+        <Header><Body><Title>menago</Title></Body></Header>
+        <Content>
+            <Button>
+                <Text>Welcome to Main page</Text>
+            </Button>
+        </Content>
+    </Container>
